23478864f9ee6f7340efde183b3ae8796e813caf
Shopop <iamgambler15@gmail.com>
2026-05-31 18:19:31 +0500

    index.js

--- index.js
+++ index.js
@@ -493,7 +493,7 @@ setInterval(async () => {
       } catch {}
     }
   }
-}, 2 * 60 * 1000);
+}, 10 * 60 * 1000);
 
 client.once('ready', () => {
   console.log(`✅ Bot online: ${client.user.tag}`);
@@ -502,4 +502,5 @@ client.once('ready', () => {
 });
 
 client.login(config.DISCORD_TOKEN);
+
 
